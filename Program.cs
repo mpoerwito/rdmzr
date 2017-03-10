3cf8a25e9a05102f60fbf4bc3b2c38881ff880fd
mpoerwito <medi.irawan@live.com>
2017-03-09 16:06:26 -0800

    latest

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
@@ -11,48 +10,51 @@ class Program
         string alpha = "abcdefghijklmnopqrstuvwxyz";
         string caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         string numeric = "0123456789";
-        string specials = "!@#$%&*-_=+{}[]";
+        string specials = "!@#$%&*-_=+[]";
+        Random rdm = new Random((int)DateTime.Now.Ticks);
 
         do {
-            Console.Write("Enter the number of characters to be generated ({0} - {1}): ", min, max);
+            Console.Write($"Enter the number of characters to be generated ({min} - {max}): ");
             if (!int.TryParse(Console.ReadLine(), out digits))
             {
-                Console.WriteLine("Please enter a whole number between {0} and {1}", min, max);
+                Console.WriteLine($"Please enter a whole number between {min} and {max}");
             }
         } while ( digits < min || digits > max);
 
-        Random rdm = new Random();
         pwd = new char[digits];
         for (int i = 0;i < digits; i++)
         {
-            char a = alpha[rdm.Next(alpha.Length)];
+            char a = alpha[GetDigit(rdm,alpha.Length)];
             // check for duplicate
             if (!Array.Exists(pwd, element => element == a) )
                 pwd.SetValue(a, i);
             else
-            {
                 i--;
-            }
         }
 
         Console.Write("Include numericals? [Y/N]: ");
         string input = Console.ReadLine();
         if (input == "Y" || input == "y")
-            pwd.SetValue(numeric[rdm.Next(numeric.Length)], rdm.Next(pwd.Length));
+            pwd.SetValue(numeric[GetDigit(rdm,numeric.Length)], GetDigit(rdm, pwd.Length));
 
         Console.Write("Include capital letters? [Y/N]: ");
         input = Console.ReadLine();
         if (input == "Y" || input == "y")
-            pwd.SetValue(caps[rdm.Next(caps.Length)], rdm.Next(pwd.Length));
+            pwd.SetValue(caps[GetDigit(rdm,caps.Length)], GetDigit(rdm, pwd.Length));
 
         Console.Write("Include special characters? [Y/N]: ");
         input = Console.ReadLine();
         if (input == "Y" || input == "y")
-            pwd.SetValue(specials[rdm.Next(specials.Length)], rdm.Next(pwd.Length));
+            pwd.SetValue(specials[GetDigit(rdm,specials.Length)], GetDigit(rdm, pwd.Length));
 
         Console.WriteLine();
         Console.WriteLine($"Generated {digits} digits password ::> {new string(pwd)}");
         Console.Write("\nPress any key to exit...");
         Console.ReadKey(true);
+    }
+
+    static int GetDigit(Random rnd, int max)
+    {
+        return rnd.Next(max);
     }
 }
